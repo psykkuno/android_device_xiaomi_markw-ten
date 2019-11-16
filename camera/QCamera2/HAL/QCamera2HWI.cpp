5abc772b06bccb29046944189010d32b718516a8
ShihabZzz <shihab.riadn@gmail.com>
2019-11-16 10:13:46 +0600

    Revert "Fix camera compilation errors"
    This reverts commit 1e8a3f2a6759cecbbed0f0f41ad0cdffb0f2d588.

--- camera/QCamera2/HAL/QCamera2HWI.cpp
+++ camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -3706,7 +3706,7 @@ int QCamera2HardwareInterface::startRecording()
 
     if (rc == NO_ERROR) {
         // Set power Hint for video encoding
-        m_perfLock.powerHint(PowerHint::VIDEO_ENCODE, true);
+        m_perfLock.powerHint(POWER_HINT_VIDEO_ENCODE, true);
     }
 
     LOGI("X rc = %d", rc);
@@ -3735,7 +3735,7 @@ int QCamera2HardwareInterface::stopRecording()
     int rc = stopChannel(QCAMERA_CH_TYPE_VIDEO);
 
     // Disable power hint for video encoding
-    m_perfLock.powerHint(PowerHint::VIDEO_ENCODE, false);
+    m_perfLock.powerHint(POWER_HINT_VIDEO_ENCODE, false);
     LOGI("X rc = %d", rc);
     return rc;
 }
