fd1c180475fdaaee1faf626cb203b69ca651fae1
ShihabZzz <shihab.riadn@gmail.com>
2019-11-26 19:26:37 +0600

    Havoc Official...

--- havoc_markw.mk
+++ havoc_markw.mk
@@ -41,3 +41,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := Xiaomi/markw/markw:6.0.1/MMB29M/V10.2.2.0.MBEMIXM:user/release-keys
+
+# Havoc
+export export HAVOC_BUILD_TYPE=Official
